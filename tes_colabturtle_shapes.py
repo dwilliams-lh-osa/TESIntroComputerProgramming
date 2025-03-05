796fa25b04061886d4d7aa6387dce5f22e3003dd
dwilliams-lh-osa <135139312+dwilliams-lh-osa@users.noreply.github.com>
2025-03-05 13:09:16 -0600

    Add files via upload

--- tes_colabturtle_shapes.py
+++ tes_colabturtle_shapes.py
@@ -1,39 +1,50 @@
+def define_constants():
+    turtle_colors = ["white", "gainsboro", "silver", "darkgray", "gray", "dimgray", "black whitesmoke",
+                    "lightgray", "violet", "orchid", "magenta", "darkviolet", "darkmagenta", "ghostwhite",
+                    "thistle", "plum", "mediumorchid", "darkorchid", "blueviolet", "purple", "aliceblue",
+                    "lavender", "lightsteelblue", "mediumpurple", "mediumslateblue", "rebeccapurple",
+                    "indigo", "lightcyan", "powderblue", "lightskyblue", "cornflowerblue", "slategray",
+                    "darkslateblue", "blue", "azure", "paleturquoise", "skyblue", "deepskyblue",
+                    "lightslategray", "slateblue", "mediumblue", "mintcream", "aquamarine", "lightblue",
+                    "darkturquoise", "dodgerblue", "royalblue", "darkblue", "honeydew", "palegreen",
+                    "lightgreen", "cyan", "cadetblue", "steelblue", "navy", "lightgoldenrodyellow", "yellow",
+                    "lawngreen", "turquoise", "mediumturquoise", "darkcyan", "midnightblue", "lightyellow",
+                    "palegoldenrod", "chartreuse", "mediumaquamarine", "lime", "teal", "darkslategray", "beige",
+                    "wheat", "greenyellow", "mediumspringgreen", "limegreen", "lightseagreen", "green", "ivory",
+                    "moccasin", "khaki", "springgreen", "darkseagreen", "mediumseagreen", "darkgreen", "lemonchiffon",
+                    "papayawhip", "gold", "yellowgreen", "goldenrod", "seagreen", "forestgreen", "cornsilk",
+                    "blanchedalmond", "tan", "darkkhaki", "darkorange", "olivedrab", "darkolivegreen", "floralwhite",
+                    "navajowhite", "burlywood", "orange", "peru", "darkgoldenrod", "olive", "oldlace", "antiquewhite",
+                    "sandybrown", "coral", "chocolate", "saddlebrown", "red", "linen", "bisque", "lightsalmon", "salmon",
+                    "orangered", "sienna", "darkred", "seashell", "peachpuff", "darksalmon", "lightcoral", "tomato", "crimson",
+                    "maroon", "snow", "mistyrose", "lightpink", "rosybrown", "indianred", "deeppink", "firebrick transparent",
+                    "lavenderblush", "pink", "hotpink", "palevioletred", "mediumvioletred", "brown"]
 
-turtle_colors = ["white", "gainsboro", "silver", "darkgray", "gray", "dimgray", "black whitesmoke",
-                 "lightgray", "violet", "orchid", "magenta", "darkviolet", "darkmagenta", "ghostwhite",
-                 "thistle", "plum", "mediumorchid", "darkorchid", "blueviolet", "purple", "aliceblue",
-                 "lavender", "lightsteelblue", "mediumpurple", "mediumslateblue", "rebeccapurple",
-                 "indigo", "lightcyan", "powderblue", "lightskyblue", "cornflowerblue", "slategray",
-                 "darkslateblue", "blue", "azure", "paleturquoise", "skyblue", "deepskyblue",
-                 "lightslategray", "slateblue", "mediumblue", "mintcream", "aquamarine", "lightblue",
-                 "darkturquoise", "dodgerblue", "royalblue", "darkblue", "honeydew", "palegreen",
-                 "lightgreen", "cyan", "cadetblue", "steelblue", "navy", "lightgoldenrodyellow", "yellow",
-                 "lawngreen", "turquoise", "mediumturquoise", "darkcyan", "midnightblue", "lightyellow",
-                 "palegoldenrod", "chartreuse", "mediumaquamarine", "lime", "teal", "darkslategray", "beige",
-                 "wheat", "greenyellow", "mediumspringgreen", "limegreen", "lightseagreen", "green", "ivory",
-                 "moccasin", "khaki", "springgreen", "darkseagreen", "mediumseagreen", "darkgreen", "lemonchiffon",
-                 "papayawhip", "gold", "yellowgreen", "goldenrod", "seagreen", "forestgreen", "cornsilk",
-                 "blanchedalmond", "tan", "darkkhaki", "darkorange", "olivedrab", "darkolivegreen", "floralwhite",
-                 "navajowhite", "burlywood", "orange", "peru", "darkgoldenrod", "olive", "oldlace", "antiquewhite",
-                 "sandybrown", "coral", "chocolate", "saddlebrown", "red", "linen", "bisque", "lightsalmon", "salmon",
-                 "orangered", "sienna", "darkred", "seashell", "peachpuff", "darksalmon", "lightcoral", "tomato", "crimson",
-                 "maroon", "snow", "mistyrose", "lightpink", "rosybrown", "indianred", "deeppink", "firebrick transparent",
-                 "lavenderblush", "pink", "hotpink", "palevioletred", "mediumvioletred", "brown"]
+    #Define variable to eliminate "magic numbers" & defaults
+    CIRCLE_DEGREES = 360
+    DEFAULT_PEN_COLOR = 'red'   #'lawngreen'
+    DEFAULT_INITIAL_SPEED = 13
+    DEFAULT_RADIUS = 300
+    DEFAULT_DRAW_PERCENT = 100
+    DEFAULT_LINE_LENGTH = 100
 
-#Define variable to eliminate "magic numbers" & defaults
-CIRCLE_DEGREES = 360
-DEFAULT_PEN_COLOR = 'red'   #'lawngreen'
-DEFAULT_INITIAL_SPEED = 13
-DEFAULT_RADIUS = 300
-DEFAULT_DRAW_PERCENT = 100
-DEFAULT_LINE_LENGTH = 100
+    sample_shape = {
+        'x': 200,
+        'y': 276,
+        'radius': 246,
+        'corners': 35,
+        'percent': DEFAULT_DRAW_PERCENT,
+        'turn_right': 0
+    }
 
-def init_turtle(initial_speed=10, pen_color='red'):
+
+def init_turtle(turtle_param, initial_speed=10, pen_color='red'):
   # _______________________________________________________________
   ''' Shorter syntax for initializing the turtle the way I want it each time '''
-  myTurtle.initializeTurtle(initial_speed)
+  module_myTurtle = turtle_param
+  module_myTurtle.initializeTurtle(initial_speed)
   #print(pen_color)
-  myTurtle.color(pen_color)
+  module_myTurtle.color(pen_color)
 
 def move_my_turtle(x,y):
   # _____________________________
@@ -43,7 +54,7 @@ def move_my_turtle(x,y):
   myTurtle.pendown()
   ##myTurtle.forward(100)
 
-help(move_my_turtle)
+#help(move_my_turtle)
 
 def draw_my_shape(corners):
 # __________________________________________
@@ -114,11 +125,7 @@ def draw_shape_dict(shape_definition):
   draw_my_shape_radius_partial(shape_definition['corners'], shape_definition['radius'],shape_definition['percent'])
 
 #help(draw_my_shape_dict)
-sample_shape = {
-    'x': 200,
-    'y': 276,
-    'radius': 246,
-    'corners': 35,
-    'percent': DEFAULT_DRAW_PERCENT,
-    'turn_right': 0
-}
+
+
+define_constants()
+printn('Module tes_colabturtle_shapes has been loaded')
